08a5c4d6cc9df0828537b61f927fef68918add01
Elgun837 <elgun.xalilov@bk.ru>
2023-04-26 15:03:38 +0400

    about section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -64,7 +64,7 @@ const ComputerCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
